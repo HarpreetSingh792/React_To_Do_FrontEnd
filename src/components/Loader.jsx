d7ba30d085d4515712cebfc4a32d109a6c5607a6
Harpreet Singh <hs.ishu000@gmail.com>
2023-08-14 09:39:50 +0530

    Motions and UI bugs are fixed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,13 +1,18 @@
-import BookLoader from "./Book.gif";
-import CircleLoader from "./Circles-menu-3.gif";
+import { useContext } from "react";
+import BookLightTheme from "./Book.gif";
+import BookDarkTheme from "./Book-DarkTheme.gif";
+import CircleLightTheme from "./Circle.gif";
+import CircleDarkTheme from "./Circle-DarkTheme.gif";
+import { Context } from "../main";
 const Loader = () => {
+  const {toastTheme} = useContext(Context);
   return (
     <div className="loading-wrapper">
       <div className="loader">
-        <img className="book-gif" src={BookLoader} alt="loader" />
+        <img className="book-gif" src={toastTheme?BookDarkTheme:BookLightTheme} alt="loader" />
         <img
           className="circles-gif"
-          src={CircleLoader}
+          src={toastTheme?CircleDarkTheme:CircleLightTheme}
           alt="loader"
           width={"80px"}
           height={"80px"}
